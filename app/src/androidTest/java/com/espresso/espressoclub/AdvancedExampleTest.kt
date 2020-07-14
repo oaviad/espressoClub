0bae83d00db7c7cc8c072b3194a382f641151ecb
Oren, Aviad (ao4296) <ao4296@intl.att.com>
2020-07-14 16:36:15 +0300

    Final commit

--- app/src/androidTest/java/com/espresso/espressoclub/AdvancedExampleTest.kt
+++ app/src/androidTest/java/com/espresso/espressoclub/AdvancedExampleTest.kt
@@ -1,49 +1,33 @@
 package com.espresso.espressoclub
 
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.typeText
-import androidx.test.espresso.matcher.ViewMatchers.assertThat
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.ext.junit.rules.activityScenarioRule
+import android.content.Intent
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.core.app.launchActivity
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.att.espressoclub.R
 import com.att.espressoclub.ui.MainActivity
-import com.att.espressoclub.ui.login.LoginActivity
-import com.espresso.espressoclub.utils.getCurrentActivity
-import org.hamcrest.CoreMatchers.instanceOf
-import org.junit.Rule
+import com.att.espressoclub.ui.login.INTENT_EXTRA_DISPLAY_NAME
+import org.junit.After
 import org.junit.Test
 import org.junit.runner.RunWith
-
-private const val USER_NAME = "test@gmail.com"
-private const val VALID_PASSWORD = "123456"
 
 @RunWith(AndroidJUnit4::class)
 class AdvancedExampleTest {
 
-    /**
-     * Use [activityScenarioRule] to create and launch the activity under test before each test,
-     * and close it after each test. This is a replacement for
-     * [androidx.test.rule.ActivityTestRule].
-     */
-    @get:Rule
-    val rule = activityScenarioRule<LoginActivity>()
+    lateinit var scenario: ActivityScenario<MainActivity>
 
     @Test
-    fun validateCurrentActivityAfterLogin() {
+    fun launchMainActivityWithLongDisplayName() {
+        val intent = Intent(ApplicationProvider.getApplicationContext(), MainActivity::class.java)
+            .putExtra(
+                INTENT_EXTRA_DISPLAY_NAME,
+                "longDisplayNamelongDisplayNamelongDisplayNamelongDisplayName"
+            )
+        scenario = launchActivity(intent)
+    }
 
-        // Type user name.
-        onView(withId(R.id.usernameEditText))
-            .perform(typeText(USER_NAME), ViewActions.closeSoftKeyboard())
-        // Type valid password.
-        onView(withId(R.id.passwordEditText))
-            .perform(typeText(VALID_PASSWORD), ViewActions.closeSoftKeyboard())
-        // Click the login button.
-        onView(withId(R.id.login)).perform(click())
-
-        // Validate the current activity is main activity
-        assertThat(getCurrentActivity(), instanceOf(MainActivity::class.java))
+    @After
+    fun cleanup() {
+        scenario.close()
     }
 }
--- app/src/androidTest/java/com/espresso/espressoclub/utils/TestUtils.kt
+++ app/src/androidTest/java/com/espresso/espressoclub/utils/TestUtils.kt
@@ -1,25 +1,10 @@
 package com.espresso.espressoclub.utils
 
-import android.app.Activity
 import android.content.Context
 import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
-import androidx.test.runner.lifecycle.ActivityLifecycleMonitorRegistry
-import androidx.test.runner.lifecycle.Stage
 
 val context: Context = getInstrumentation().targetContext
 
 fun getString(id: Int): String? {
     return context.resources.getString(id)
-}
-
-fun getCurrentActivity(): Activity? {
-    var currentActivity: Activity? = null
-    getInstrumentation().runOnMainSync {
-        run {
-            currentActivity =
-                ActivityLifecycleMonitorRegistry.getInstance().getActivitiesInStage(Stage.RESUMED)
-                    .elementAtOrNull(0)
-        }
-    }
-    return currentActivity
 }
